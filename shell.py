ac7b5932da176f1a7ab1c430e7f99887c5e3a2d9
Miles Punch <71351127+miles-p@users.noreply.github.com>
2023-03-23 08:32:48 +1100

    Initial commit

---
+++ shell.py
@@ -0,0 +1,75 @@
+import sacn
+import time
+
+universe_1 = [
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+]
+fixture_groups = [
+    [1,2,3,4,5,6,7,8],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[]
+]
+
+sender = sacn.sACNsender()  # provide an IP-Address to bind to if you want to send multicast packets from a specific interface
+sender.start()  # start the sending thread
+sender.activate_output(1)  # start sending out data in the 1st universe
+sender[1].multicast = True  # set multicast to True
+
+
+class DirectOperation:
+    def AppendUni(fixtures, universe, level):
+        for singleFixture in fixtures:
+            try:
+                universe[singleFixture-1] = level
+            except IndexError:
+                ErrorHandler.RaiseError("At least one of these fixtures is out of range, please try a different selection.")
+
+class ErrorHandler:
+    def RaiseError(text):
+        print(text)
+
+class OutputManagement:
+    def sendPackets(data):
+        sender[1].dmx_data = data
+
+DirectOperation.AppendUni(fixture_groups[0],universe_1,50)
+print(universe_1)
+OutputManagement.sendPackets(universe_1)
