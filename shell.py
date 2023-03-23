9e54dfb0c53fe6a55097b955e7761a1f177c3032
Miles Punch <71351127+miles-p@users.noreply.github.com>
2023-03-23 11:02:48 +1100

    Selection now works with groups!

--- shell.py
+++ shell.py
@@ -37,7 +37,7 @@ universe_1 = [
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 ]
 fixture_groups = [
-    [1,2,3,4,5,6,7,8],[],[],[],[],[],[],[],[],[],
+    [],[],[],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],[],[],
@@ -48,7 +48,7 @@ fixture_groups = [
     [],[],[],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],[],[]
 ]
-
+cue_stack = []
 sender = sacn.sACNsender(fps=30)  # provide an IP-Address to bind to if you want to send multicast packets from a specific interface
 sender.start()  # start the sending thread
 sender.activate_output(1)  # start sending out data in the 1st universe
@@ -77,6 +77,11 @@ while True:
     print(cmd)
     for items in cmd:
          if items == 'at':
-              DirectOperation.AppendUni([int(cmd[stepCount])],universe_1,int(cmd[stepCount+2]))
-              print([int(cmd[stepCount])])
+              if cmd[stepCount].startswith("g"):
+                   selection = fixture_groups[int(cmd[stepCount][1:])]    #[cmd[stepCount][1:]]
+                   #print(fixture_groups[int(cmd[stepCount][1:])],int(cmd[stepCount][1:]))
+              else:
+                   selection = [int(cmd[stepCount])]
+              DirectOperation.AppendUni(selection,universe_1,int(cmd[stepCount+2]))
+              #print([int(cmd[stepCount])])
     stepCount = stepCount + 1
